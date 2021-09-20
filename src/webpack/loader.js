679f7d098376aae6c4d4158faaf592c2036f885e
Ken Kubiak <ken@mnmlst.me>
2021-09-19 22:46:01 -0700

    load react or elm via webpack
    * Emits intermediate files.
    * Still requires rules for jsx and elm in webpack.config.js

--- src/webpack/loader.js
+++ src/webpack/loader.js
@@ -1,46 +1,64 @@
 const { getOptions } = require('loader-utils')
-
+const fs = require('fs')
 const path = require('path')
 const Kr = require('../index.js')
 
 function loader(content) {
     const basename = path.basename(this.resourcePath, ".kr")
+    const dir = path.dirname(this.resourcePath)
+    const subdir = path.join(dir, basename)
+
     const options = getOptions(this) || {}
     const defaults = options.defaults || {}
     const callback = this.async()
-    const q = this.resourceQuery
 
     console.log("Kram loader options: ", options)
-    console.log("Kram loader query: ", q)
 
-    if ( q ) {
-      // dekram workbook for a specific platform & language
+    const getConfig = platform =>
+      getPlatformByName(platform, options.platforms)
 
-      console.log("Dekram: ", q, content)
-
-      const matches = q.match(/^\?dekram=(([^:]+):)?(\w+)$/)
-      // const matches = dekram.match(/^(([^:]+):)?(\w+)$/)
-      const platform = matches && matches[2] || defaults.platform || 'html'
-      const language = matches && matches[3] || defaults.language || 'html'
-      const config = getPlatformByName(options.platforms, platform)
-
-      const plugin = loadPlugin(config, this.rootContext)
-
-      parse(content, basename)
-        .then(wb => dekram(wb, language, this.emitFile, plugin))
-        .then(component => callback(null, component))
-        .catch(callback)
-
-    } else {
-      // initial parse of markdown and frontMatter
-
-      parse(content, basename)
-        .then(wb => collect(wb, options, this.resourcePath, this.rootContext))
-        .then(component => callback(null, component))
-        .catch(callback)
-
+    const defaultPlugin = {
+      collate: function (workbook, lang) {
+        const chunks = Kr.extract(workbook, lang)
+        return chunks.map(t => t.text).join('\n')
+      }
     }
 
+    const getPlugin = platform => {
+      const config = getConfig(platform)
+
+      if (config.plugin) {
+        const pluginPath = path.resolve(this.rootContext, config.plugin)
+        return require(pluginPath)
+      } else {
+        return defaultPlugin
+      }
+    }
+
+    const emit = (name, code) => {
+      const filename = path.join(subdir, name)
+      fs.mkdirSync(subdir, {recursive: true})
+      fs.writeFileSync(filename, code)
+      return filename
+    }
+
+    // initial parse of markdown and frontMatter
+
+    parse(content, basename)
+      .then(wb => dekram(wb, getConfig(wb.platform), emit, getPlugin(wb.platform)))
+      .then(wb => collect(wb, getConfig(wb.platform)))
+      .then(component => callback(null, component))
+      .catch(callback)
+}
+
+function getPlatformByName(name, list) {
+  const matching = list.filter( p => p.name === name )
+
+  if (matching.length === 1) {
+    return matching[0]
+  } else {
+    return {}
+  }
 }
 
 function parse(content, basename) {
@@ -55,67 +73,46 @@ function parse(content, basename) {
   })
 }
 
-const defaultPlugin = {
-  collate: function (workbook, lang) {
-    const chunks = Kr.extract(workbook, lang)
-    return chunks.map(t => t.text).join('\n')
-  }
-}
-
-function loadPlugin(config, rootContext) {
-  // TODO: use module resolution for plugins
-  const pluginPath = path.resolve(rootContext, config.plugin)
-  return require(pluginPath)
-}
-
-function dekram(workbook, lang, emitter, plugin=defaultPlugin) {
-  const { moduleName } = workbook
+function dekram(workbook, config, emitter, plugin=defaultPlugin) {
+  const { moduleName, languages } = workbook
   const { collate } = plugin
-  const filename = [moduleName, lang].join('.')
-  const code = collate(workbook, lang)
 
-  return emitter(filename, code)
-
-}
-
-function collect(workbook, options, resourcePath, rootContext) {
-  const json = JSON.stringify( workbook )
-  const {platforms, root} = options
-  const {basename, moduleName, platform, languages} = workbook
-  const config = getPlatformByName(platforms, platform)
-  const plugin = loadPlugin(config, rootContext)
+  const emit = lang => {
+    const {name, language, code} = collate(workbook, lang)
+    return {
+      language,
+      filepath: emitter(name, code),
+    }
+  }
 
   const modules = config.modules
     .filter( m => languages.find(s => s === m.language) )
-    .map( m => ({
-      language: m.language,
-      pipeline: m.use,
-      query: `dekram=${platform}:${m.language}`
-    }))
+    .map( m => Object.assign(
+      emit(m.language),
+      {
+        pipeline: m.use,
+        bind: plugin.bind(moduleName, m.language)
+      }
+    ))
+
+  return Object.assign({modules}, workbook)
+}
+
+function collect(workbook, config) {
+  const {modules} = workbook
+  const json = JSON.stringify(Object.assign(workbook, {modules: "TBD"}))
 
   const loader = m =>
-    `() => import(\`${m.pipeline}!${resourcePath}?${m.query}\`)`
-
-  const binder = m => plugin.bind(moduleName, m.language)
+    `function () { return import(\`!${m.pipeline}!${m.filepath}\`) }`
 
   const resourceDefn = m =>
-    `{language: "${m.language}", load: ${loader(m)}, bind: ${binder(m)} }`
+    `{language: "${m.language}", load: ${loader(m)}, bind: ${m.bind} }`
 
   const definitions = modules.map(resourceDefn).join(",\n")
 
-  return `export default Object.assign(
-    {modules: [${definitions}]},
-    ${json})`
-}
+  console.log("Kram module definitions: ", definitions)
 
-function getPlatformByName(list, name) {
-  const matching = list.filter( p => p.name === name )
-
-  if (matching.length === 1) {
-    return matching[0]
-  } else {
-    throw `no platform found for ${name}`
-  }
+  return `export default Object.assign(${json},{modules: [${definitions}]})`
 }
 
 module.exports = loader
