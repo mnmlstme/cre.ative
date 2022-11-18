b52a9aece6dee1ef0c948ff9f8ce83ef75b36abd
Ken Kubiak <ken@mnmlst.me>
2022-11-17 16:09:45 -0800

    version 1.3.4

--- src/loader.js
+++ src/loader.js
@@ -20,9 +20,6 @@ function loader(content) {
   const platform = workbook.platform || "web-standard";
   const plugin = platforms[platform] || Kr.defaultPlugin;
   const dir = path.join(platform, projname, basename);
-  const outdir = output ? path.join(output, dir) : dir;
-
-  const epilog = ""; //if (module.hot) { module.hot.decline() }";
 
   console.log("Kram loader, using plugin:", platform, projname, basename);
 
@@ -30,43 +27,23 @@ function loader(content) {
     resolve({ project: projname, ...workbook })
   )
     .then((wb) => Kr.classify(wb, plugin.modules))
-    .then((wb) => Kr.dekram(wb, emitter(outdir), plugin))
-    .then((wb) => Kr.collect(wb, loadFn, epilog))
+    .then((wb) => Kr.dekram(wb, emitter(dir, output), plugin))
+    .then((wb) => Kr.collect(wb))
     .then((mod) => callback(null, mod))
     .catch(callback);
 
-  function loadFn({ filepath }) {
-    const targetPath = path.relative(projpath, filepath);
-    const acceptPath = path.relative(root, filepath);
+  function emitter(reldir, output = "") {
+    const absdir = path.join(output, reldir);
 
-    return `function ( onHotSwap ) { 
-      const accept = './${acceptPath}';
-      const enableHMR = (mod) => {
-        if (module.hot) {
-          if (onHotSwap) { 
-            module.hot.accept(accept, onHotSwap);
-            console.log('Enabled Hot-Swap on module:', accept, mod);
-          } else {
-            console.log('Hot-Swap available but declined for module:', accept);
-            module.hot.decline(accept)
-          }
-        }
-        return mod;
-      }
-      return import( /* webpackMode: "lazy" */ '${targetPath}' )
-        .then(enableHMR)
-    }`;
-  }
-
-  function emitter(outdir) {
     return (name, code) => {
-      const filename = path.join(outdir, name);
+      const relname = path.join(reldir, name);
+      const absname = path.join(absdir, name);
 
-      fs.mkdirSync(outdir, { recursive: true });
-      fs.writeFileSync(filename, code);
+      fs.mkdirSync(absdir, { recursive: true });
+      fs.writeFileSync(absname, code);
 
-      // console.log("Kram emit", filename);
-      return filename;
+      console.log(`Kram emit: ${relname} -> `, absname);
+      return relname;
     };
   }
 }
