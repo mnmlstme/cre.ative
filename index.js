c34db133aa3dac0afffbbfb5c9909f860cd95b0d
Ken Kubiak <ken@mnmlst.me>
2023-04-28 12:59:42 -0700

    run webpack from 11ty

--- index.js
+++ index.js
@@ -1,32 +1,82 @@
 const Kr = require("@cre.ative/kram");
-const he = require("he");
 const path = require("node:path");
-const Prism = require("prismjs");
-const loadLanguages = require("prismjs/components/");
-const webStandard = require("./web-standard-plugin");
+const { open } = require("node:fs/promises");
+const Kwp = require("@cre.ative/kram-express-webpack");
+const { render } = require("./render.js");
 
 module.exports = {
   configure,
 };
 
-function configure() {
-  const options = {
-    platforms: {
-      "web-standard": Kr.register(webStandard),
-    },
-    defaultPlatform: "web-standard",
+function configure(options = {}) {
+  const { input = "./src", output = "./docs", template, platforms } = options;
+  const basedir = process.cwd();
+  const inputRoot = path.resolve(basedir, input);
+  const outputRoot = path.resolve(basedir, output);
+
+  const config = {
+    basedir,
+    docroot: inputRoot,
+    public: outputRoot,
+    ...options,
   };
 
   return {
     outputFileExtension: "html",
     compile: async function (inputContent, inputPath) {
-      const workbook = parse(inputContent, inputPath);
+      const basename = path.basename(inputPath, ".md");
+      const relPath = path.relative(inputRoot, path.dirname(inputPath));
+      const webpack = await Kwp.packager({
+        entry: {
+          workbook: {
+            import: inputPath,
+            filename: path.join(relPath, basename, "workbook.js"),
+          },
+        },
+        ...config,
+      });
 
       return async (data) => {
         if (data.platform === "none") {
           return this.defaultRenderer(data);
         } else {
-          return render(workbook, data, options);
+          const plugin = Kr.register(require(platforms[data.platform]));
+          const workbook = Kr.parse(
+            { body: inputContent, attributes: data },
+            basename
+          );
+          const html = render(workbook, data, plugin);
+
+          return await new Promise((resolve, reject) => {
+            webpack.run((err, stats) => {
+              if (err) {
+                reject(err);
+                return;
+              }
+
+              resolve(html);
+
+              if (stats.hasErrors()) {
+                const info = stats.toJson();
+                console.error(
+                  `[kram] ERROR (webpack) ${inputPath}\n`,
+                  info.errors
+                    .map((e) => `[kram] E      ${e.message}`)
+                    .join("\n")
+                );
+              }
+
+              if (stats.hasWarnings()) {
+                const info = stats.toJson();
+                console.warn(
+                  `[kram] WARNING (webpack) ${inputPath}\n`,
+                  info.warnings
+                    .map((w) => `[kram] W      ${w.message}`)
+                    .join("\n")
+                );
+              }
+            });
+          });
         }
       };
     },
@@ -37,201 +87,4 @@ function parse(inputContent, inputPath) {
   const basename = path.basename(inputPath, ".md");
   const projname = path.dirname(inputPath);
   return Kr.parse(inputContent, basename);
-}
-
-function render(wb, data, options) {
-  const { platforms, defaultPlatform } = options;
-  const { platform = defaultPlatform } = data;
-  const plugin = platforms[platform] || {};
-  let files = {};
-
-  wb = Kr.classify(wb, plugin.modules);
-  wb = Kr.dekram(wb, emitter, plugin);
-
-  return genHtml(wb, data, files);
-
-  function emitter(name, code) {
-    files[name] = code;
-    return name;
-  }
-}
-
-function genHtml(wb, data, files) {
-  const { basename, scenes, modules = [], plugin } = wb;
-  const { title, model } = data;
-  const namedModules = modules.filter((mdl) => Boolean(mdl.moduleName));
-  const mountScript = namedModules.length
-    ? `
-import { mount } from "/scripts/oper.ative.js";
-console.log("Mounting Operative");
-const mountpoint = document.getElementById("lets-be-oper-ative");
-const initial = ${JSON.stringify(model)};
-const moduleNames = ${JSON.stringify(
-        namedModules.map((mdl) => mdl.moduleName)
-      )};
-moduleNames.forEach((name) => mount(name, mountpoint, initial));
-`
-    : "";
-
-  return `<!DOCTYPE html>
-<html>
-<head>
-<title>${title}</title>
-<link rel="stylesheet" href="/styles/prism.css" webc:keep>
-<link rel="stylesheet" href="/styles/theme.css" webc:keep>
-${genCSSDefs(modules, files)}
-${genJSDefs(modules, files)}
-</head>
-<body webc:bucket="defer">
-${genHTMLDefs(modules, files)}
-${genSVGDefs(modules, files)}
-<st-ative>
-<navig-ative>
-<span slot="title">${title}</title>
-</navig-ative>
-<consolid-ative>
-${genScenes(scenes, modules, files)}
-</consolid-ative>
-</st-ative>
-<script type="module" webc:keep>
-${mountScript}
-</script>
-</body>
-</html>
-`;
-}
-
-function genCSSDefs(modules, files) {
-  const blocks = modules
-    .filter((mdl) => mdl && mdl.language === "css")
-    .map((mdl) => files[mdl.filepath]);
-
-  return blocks.map((blk) => `<style>${blk}</style>`).join("\n");
-}
-
-function genJSDefs(modules, files) {
-  const blocks = modules
-    .filter((mdl) => mdl && mdl.language === "js")
-    .map((mdl) => files[mdl.filepath]);
-
-  return blocks
-    .map((blk) => `<script type="module">${blk}</script>`)
-    .join("\n");
-}
-
-function genHTMLDefs(modules, files) {
-  const blocks = modules
-    .filter((mdl) => mdl && mdl.language === "html" && mdl.mode === "define")
-    .map((mdl) => files[mdl.filepath])
-    .map((code) =>
-      code.replaceAll(/<\s*template(\s+[^>]*)?>/gi, "<template webc:raw$1>")
-    );
-
-  return blocks.join("\n");
-}
-
-function genSVGDefs(modules, files) {
-  const blocks = modules
-    .filter((mdl) => mdl && mdl.language === "svg" && mdl.mode === "define")
-    .map((mdl) => files[mdl.filepath]);
-
-  return blocks.join("\n");
-}
-
-function genScenes(scenes, modules, files) {
-  const filesByScene = Object.fromEntries(
-    modules
-      .filter((mdl) => mdl.scene)
-      .map(({ scene, filepath }) => [scene, files[filepath]])
-  );
-  const sceneBlocks = scenes.map(({ blocks }, i) => {
-    const file = filesByScene[i + 1] || [];
-    const prose = blocks
-      .filter((blk) => !isEvalBlock(blk))
-      .map(genProse)
-      .join("\n");
-    const evalcode = blocks
-      .filter(isEvalBlock)
-      .map(([_, { lang }, ...rest]) => {
-        const code = rest.join("\n");
-        const formatted = Prism.highlight(code, Prism.languages[lang], lang);
-        return `<code lang="${lang}" class="language-${lang}">${formatted}</code>`;
-      })
-      .join("\n");
-
-    return [
-      `<oper-ative>${file}</oper-ative>`,
-      `<ide-ative data-language="auto">${evalcode}</ide-ative>`,
-      `<narr-ative>${prose}</narr-ative>`,
-    ];
-  });
-
-  return sceneBlocks.flat().join("\n");
-}
-
-function isEvalBlock(blk) {
-  const [type, attr = {}] = blk;
-
-  return type === "fence" && attr.mode === "eval";
-}
-
-function genProse(blk) {
-  const [type, { tag, lang }, ...rest] = blk;
-
-  // console.log(`genProse(${type}, {tag:${tag}}, ... ${rest.length} inside)`);
-
-  switch (type) {
-    case "html_block":
-      return rest.join("\n");
-    case "fence":
-      const code = rest.join("\n");
-      const formatted = Prism.highlight(code, Prism.languages[lang], lang);
-      return `<ide-ative data-language="${lang}"><code class="language-${lang}">${formatted}</code></ide-ative>`;
-    case "bullet_list":
-    case "ordered_list":
-    case "list_item":
-      return `<${tag}>${rest.map(genProse).join("\n")}</${tag}>\n`;
-    default:
-      return `<${tag}>${jsonToHtml(rest)}</${tag}>\n`;
-  }
-}
-
-function jsonToHtml(tokens) {
-  // console.log("jsonToHtml", JSON.stringify(tokens, null, ""));
-
-  const out = tokens
-    .map((t) => (typeof t === "string" ? encodeAsHtml(t) : tokenToHtml(t)))
-    .join("");
-
-  // console.log("jsonToHtml", JSON.stringify(tokens, null, ""));
-  // console.log(" --->", out);
-
-  return out;
-}
-
-function encodeAsHtml(text) {
-  return he.encode(text, {
-    useNamedReferences: true,
-    decimal: true,
-  });
-}
-
-function tokenToHtml([type, attrs, ...children]) {
-  const { tag, block, markup, href } = attrs;
-  if (type === "html_inline") {
-    // console.log("HTML out:", JSON.stringify(children));
-    return children.join("\n");
-  }
-  const hrefPair = href && ["href", href];
-  const markPair = markup &&
-    !type.match(/\w+_list/) &&
-    markup != "" && [`data-mark-${block ? "before" : "around"}`, markup];
-  const htmlAttrs = [markPair, hrefPair]
-    .filter(Boolean)
-    .map(([k, v]) => ` ${k}="${v}"`)
-    .join("");
-
-  return children
-    ? `<${tag}${htmlAttrs}>${jsonToHtml(children)}</${tag}>`
-    : `<${tag}${htmlAttrs}/>`;
 }
---
+++ render.js
@@ -0,0 +1,149 @@
+const he = require("he");
+const Kr = require("@cre.ative/kram");
+
+module.exports = { render };
+
+function render(wb, data, plugin) {
+  let files = {};
+
+  wb = Kr.classify(wb, plugin.modules);
+  wb = Kr.dekram(wb, emitter, plugin);
+
+  return genHtml(wb, data, files);
+
+  function emitter(name, code) {
+    console.log("[kram-11ty] emit:", name);
+    files[name] = code;
+    return name;
+  }
+}
+
+function genHtml(wb, data, files) {
+  const { basename, scenes, modules = [] } = wb;
+  const { title, model } = data;
+  const namedModules = modules.filter((mdl) => Boolean(mdl.moduleName));
+  console.log(
+    "Generating Html",
+    modules.map((m) => m.moduleName).join(","),
+    Object.keys(files).join(",")
+  );
+
+  return `<!DOCTYPE html>
+<html>
+<head>
+<title>${title}</title>
+<link rel="stylesheet" href="/styles/theme.css" webc:keep>
+</head>
+<body webc:bucket="defer">
+<kram-main>
+<kram-nav>
+<span slot="title">${title}</title>
+</kram-nav>
+<kram-flow>
+${genScenes(scenes, modules, files)}
+</kram-flow>
+</kram-main>
+<script type="module" webc:keep>
+import Wb from "./workbook.js";
+console.log("Workbook loaded; ready to mount!", wb);
+</script>
+</body>
+</html>
+`;
+}
+
+function genScenes(scenes, modules, files) {
+  const filesByScene = Object.fromEntries(
+    modules
+      .filter((mdl) => mdl.scene)
+      .map(({ scene, filepath }) => [scene, files[filepath]])
+  );
+  const sceneBlocks = scenes.map(({ blocks }, i) => {
+    const file = filesByScene[i + 1] || [];
+    const prose = blocks
+      .filter((blk) => !isEvalBlock(blk))
+      .map(genProse)
+      .join("\n");
+    const evalcode = blocks
+      .filter(isEvalBlock)
+      .map(([_, { lang }, ...rest]) => {
+        const code = rest.join("\n");
+        return `<code lang="${lang}" class="language-${lang}">${code}</code>`;
+      })
+      .join("\n");
+
+    return [
+      `<kram-scene>${file}</kram-scene>`,
+      `<kram-code data-language="auto">${evalcode}</kram-code>`,
+      `<kram-narrative>${prose}</kram-narrative>`,
+    ];
+  });
+
+  return sceneBlocks.flat().join("\n");
+}
+
+function isEvalBlock(blk) {
+  const [type, attr = {}] = blk;
+
+  return type === "fence" && attr.mode === "eval";
+}
+
+function genProse(blk) {
+  const [type, { tag, lang }, ...rest] = blk;
+
+  // console.log(`genProse(${type}, {tag:${tag}}, ... ${rest.length} inside)`);
+
+  switch (type) {
+    case "html_block":
+      return rest.join("\n");
+    case "fence":
+      const code = rest.join("\n");
+      return `<kram-code data-language="${lang}"><code class="language-${lang}">${code}</code></kram-code>`;
+    case "bullet_list":
+    case "ordered_list":
+    case "list_item":
+      return `<${tag}>${rest.map(genProse).join("\n")}</${tag}>\n`;
+    default:
+      return `<${tag}>${jsonToHtml(rest)}</${tag}>\n`;
+  }
+}
+
+function jsonToHtml(tokens) {
+  // console.log("jsonToHtml", JSON.stringify(tokens, null, ""));
+
+  const out = tokens
+    .map((t) => (typeof t === "string" ? encodeAsHtml(t) : tokenToHtml(t)))
+    .join("");
+
+  // console.log("jsonToHtml", JSON.stringify(tokens, null, ""));
+  // console.log(" --->", out);
+
+  return out;
+}
+
+function encodeAsHtml(text) {
+  return he.encode(text, {
+    useNamedReferences: true,
+    decimal: true,
+  });
+}
+
+function tokenToHtml([type, attrs, ...children]) {
+  const { tag, block, markup, href } = attrs;
+  if (type === "html_inline") {
+    // console.log("HTML out:", JSON.stringify(children));
+    return children.join("\n");
+  }
+  const hrefPair = href && ["href", href];
+  const markPair = markup &&
+    !type.match(/\w+_list/) &&
+    markup != "" && [`data-mark-${block ? "before" : "around"}`, markup];
+  const htmlAttrs = [markPair, hrefPair]
+    .filter(Boolean)
+    .map(([k, v]) => ` ${k}="${v}"`)
+    .join("");
+
+  return children
+    ? `<${tag}${htmlAttrs}>${jsonToHtml(children)}</${tag}>`
+    : `<${tag}${htmlAttrs}/>`;
+}
